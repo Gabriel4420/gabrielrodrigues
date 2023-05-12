cef4d6e97dcb9d5d0b11cffd28e858fe336b8d78
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2023-05-12 00:11:39 -0300

    fix: :bug: corrigindo logo do header

--- src/components/Atoms/Navs/NavMenuMobile.tsx
+++ src/components/Atoms/Navs/NavMenuMobile.tsx
@@ -16,7 +16,7 @@ const NavMenuMobile: FC<PropertiesNavMenuIcon> = ({
         <div>
           <div className="flex w-full items-center justify-between ">
             <Image
-              src="/../assets/logogabriel.png"
+              src="/assets/logogabriel.png"
               alt="logo"
               width="87"
               height="25"
