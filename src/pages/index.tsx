994bcb6e2d94ddcb2b724b3c03d171e87533a536
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2023-05-12 13:21:25 -0300

    feat: :sparkles: finalizando metatags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,12 +25,48 @@ export default function Home({ dataSummaryProjects, allTechnologies }: any) {
     <>
       <Head>
         <title>Gabriel Rodrigues: Fullstack Developer </title>
+        <meta name="title" content="Gabriel Rodrigues: Fullstack Developer" />
         <meta
           name="description"
           content="Gabriel Rodrigues Perez: Desenvolvedor Fullstack"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+
+        {/* Facebook */}
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://gabrielrodrigues.vercel.app/"
+        />
+        <meta
+          property="og:title"
+          content="Gabriel Rodrigues: Fullstack Developer"
+        />
+        <meta
+          property="og:description"
+          content="Gabriel Rodrigues Perez: Desenvolvedor Fullstack"
+        />
+        <meta property="og:image" content="/assets/logogabriel.png" />
+
+        {/* Twitter */}
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://gabrielrodrigues.vercel.app/"
+        />
+        <meta
+          property="twitter:title"
+          content="Gabriel Rodrigues: Fullstack Developer"
+        />
+        <meta
+          property="twitter:description"
+          content="Gabriel Rodrigues Perez: Desenvolvedor Fullstack"
+        />
+        <meta property="twitter:image" content="/assets/logogabriel.png" />
+
+        <link rel="icon" type="image/png" href="/assets/logogabriel.png" />
       </Head>
 
       <MainHero />
